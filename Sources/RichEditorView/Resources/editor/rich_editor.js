ed734f6f9bb2bb2e09efe183764b488d50d3bcf1
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-07-07 23:45:23 -0400

    Togglable heading

--- Sources/RichEditorView/Resources/editor/rich_editor.js
+++ Sources/RichEditorView/Resources/editor/rich_editor.js
@@ -196,7 +196,8 @@ RE.setTextBackgroundColor = function(color) {
 };
 
 RE.setHeading = function(heading) {
-    document.execCommand('formatBlock', false, '<h' + heading + '>');
+    var sel = document.getSelection().getRangeAt(0).startContainer.parentNode;
+    document.execCommand('formatBlock', false, sel.tagName === `H${heading}` ? '<p>' : `<h${heading}>`);
 };
 
 RE.setIndent = function() {
